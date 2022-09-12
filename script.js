5c1c7e67bd9e73627cdfe181c98a742411962f61
Danilo Argolo <dan_argolo@hotmail.com>
2022-09-12 13:13:35 -0300

    Resolvendo as classes

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const text = document.querySelector('#carta-texto');
 const letter = document.querySelector('#carta-gerada');
 
+
 const btnText = document.createElement('button');
 document.querySelector('.inputs').appendChild(btnText);
 btnText.setAttribute('type', 'button');
 btnText.id = 'criar-carta';
 
 function insert() {
+  if (text.value.match(/^(\s+)$/gm)) {
+    letter.innerHTML = 'Por favor, digite o conteúdo da carta.';
+    console.log(letter.innerText);
+    }
   letter.innerHTML = text.value.replace(/(\S+)/gm, '<span>$1</span>');
 }
+  
+
 btnText.addEventListener('click', insert);
+const span = document.getElementsByTagName('span');
