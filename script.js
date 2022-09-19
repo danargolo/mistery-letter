0a8614e163f332f77aa35d858c47f31f87b66e48
Danilo Argolo <dan_argolo@hotmail.com>
2022-09-19 19:33:22 -0300

    Resolvendo 5, 16 e bônus

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
 const text = document.querySelector('#carta-texto');
 const letter = document.querySelector('#carta-gerada');
 
-
 const btnText = document.createElement('button');
 document.querySelector('.inputs').appendChild(btnText);
 btnText.setAttribute('type', 'button');
 btnText.id = 'criar-carta';
 
-function insert() {
-  if (text.value.match(/^(\s+)$/gm)) {
-    letter.innerHTML = 'Por favor, digite o conteúdo da carta.';
-    console.log(letter.innerText);
-    }
-  letter.innerHTML = text.value.replace(/(\S+)/gm, '<span>$1</span>');
-}
-  
+const design = ['newspaper', 'magazine1', 'magazine2'];
+const size = ['medium', 'big', 'reallybig'];
+const rotate = ['rotateleft', 'rotateright'];
+const incline = ['skewleft', 'skewright'];
 
-btnText.addEventListener('click', insert);
-const span = document.getElementsByTagName('span');
+function removeSpan() {
+  const spanSz = document.querySelectorAll('span');
+  if (spanSz.length >= 1) {
+    for (let index = spanSz.length - 1; index >= 0; index -= 1) {
+      spanSz[index].remove();
+    }
+  }
+}
+
+function insert() {
+  if (text.value === '' || text.value.match(/^(\s+)$/gm)) {
+    letter.innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    letter.innerText = '';
+    const phrase = text.value.trim();
+    const words = phrase.split(' ');
+    document.getElementById('carta-contador').innerText = words.length;
+    for (let index = 0; index < words.length; index += 1) {
+      const span = document.createElement('span');
+      span.innerText = words[index];
+      document.getElementById('carta-gerada').appendChild(span);
+    }
+  }
+}
+
+function sty(param) {
+  const selector = Math.floor(Math.random() * param.length);
+  return param[selector];
+}
+
+function spanClass() {
+  const spanSz = document.querySelectorAll('span');
+  for (let index = 0; index < spanSz.length; index += 1) {
+    spanSz[index].className = `${sty(design)} ${sty(size)} ${sty(rotate)} ${sty(incline)}`;
+
+    spanSz[index].addEventListener('click', () => {
+      spanSz[index].className = `${sty(design)} ${sty(size)} ${sty(rotate)} ${sty(incline)}`;
+    });
+  }
+}
+
+btnText.addEventListener('click', () => {
+  removeSpan();
+  insert();
+  spanClass();
+});
